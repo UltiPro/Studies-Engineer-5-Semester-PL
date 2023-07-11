22b8bb2f5b930c4b28e88ab307b1de6a69157cfb
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-07-11 02:08:13 +0200

    Repository refresh

---
+++ Systemy Mobilne/Pracownia Specjalistyczna nr 9/SM-02-localization/ViewController.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property(nonatomic, weak) IBOutlet UIButton *informationButton;
+@property(nonatomic, weak) IBOutlet UIImageView *image;
+- (IBAction)click;
+@end
