2f6c9d13f84eeab7f0aff85b2f076b2acc8f100b
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-07-11 03:22:28 +0200

    Repository refresh

---
+++ Systemy Mobilne/Pracownia Specjalistyczna nr 1/app/src/main/java/ulti/quiz/Question.java
@@ -0,0 +1,27 @@
+package ulti.quiz;
+
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+
+    public Question(int questionId, boolean answer) {
+        this.questionId = questionId;
+        this.trueAnswer = answer;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public boolean isTrueAnswer() {
+        return trueAnswer;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public void setAnswer(boolean answer) {
+        this.trueAnswer = answer;
+    }
+}
